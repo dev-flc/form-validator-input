b9888be469ebba23a1f6a258214b8070de75d5a9
Fernando L.C <fer.lc.developer@gmail.com>
2022-08-18 20:52:13 -0500

    hay un console

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ export { singleValidation } from 'Funtion/singleValidation.js';
 export { multiValidation } from 'Funtion/multiValidation.js';
 
 export { multiErrorsValidation } from 'Funtion/multiErrorsValidation.js';
+
+
+// console.log("Hola");
